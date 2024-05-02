0405bdf06f218ad3b70ab906fa093062954cae0e
Shyamarjun10 <Shyamarjuna102k4@gmail.com>
2024-05-02 21:18:10 +0530

    added userupdate file1

--- aucsite/src/components/login.js
+++ aucsite/src/components/login.js
@@ -2,7 +2,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import React, { useState } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
-import "/Users/amvis/Desktop/DBMS-Auction/DBMS-AUCTION/aucsite/src/styles/login.css"; // Assuming you have your custom styles in this file
+//import "/Users/amvis/Desktop/DBMS-Auction/DBMS-AUCTION/aucsite/src/styles/login.css"; // Assuming you have your custom styles in this file
 
 function Login() {
   const [formData, SetFormData] = useState({
--- aucsite/src/components/userupdate.js
+++ aucsite/src/components/userupdate.js
@@ -1,24 +1,53 @@
-import axios from axios;
+import React, { useState } from "react";
+import axios from "axios";
 
-class User {
-  static async createUser(formData) {
+function Register() {
+  const [formData, setFormData] = useState({
+    username: "",
+    email: "",
+    password: "",
+    firstname: "",
+    lastname: "",
+    address: "",
+    address2: "",
+    contact: "",
+    city: "",
+    state: "",
+    zipcode: "",
+  });
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
     try {
       console.log("data:", formData);
-      const response = await axios.post("http://localhost:3001/user/register", formData);
+      const response = await axios.post(
+        "http://localhost:3001/user/register",
+        formData
+      );
       console.log("User registered successfully:", response.data);
-      return response.data; // Assuming the response contains the newly created user data
     } catch (err) {
-      console.error("Error creating a new user:", err);
-      throw err;
+      console.error("Error registering user:", err);
     }
-  }
+  };
+
+  return (
+    <div className="container-fluid bg-light text-dark">
+      <h1 className="text-black text-center">Register</h1>
+      <form
+        className="row justify-content-center mt-3 mx-auto w-75 text-start"
+        onSubmit={handleSubmit}
+      >
+        {/* Form fields go here */}
+        <button type="submit" className="btn btn-primary mb-5 w-25">
+          Register
+        </button>
+      </form>
+    </div>
+  );
 }
-static async viewUsers() {
-    try {
-      const response = await axios.get("http://localhost:3001/user/viewUsers");
-      return response.data; // Assuming the response contains the list of users
-    } catch (err) {
-      console.error("Error fetching the users:", err);
-      throw err;
-    }
-}
+
+export default Register;
